1b931cac434ec52a510126bcc9da5842d38ec86a
koalasushi <kai.daniel.travis@gmail.com>
2026-05-29 18:23:07 +0900

    dynamic rendering

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
 from fastapi.staticfiles import StaticFiles
 
+from pydantic import BaseModel
+from datetime import datetime
+
 from db import conn
 
 app = FastAPI()
@@ -36,3 +39,82 @@ def home(request: Request):
             "cell_lines": cell_lines
         }
     )
+
+class VialCreate(BaseModel):
+    cell_line: str
+    passage_number: int
+    frozen_by: str
+    notes: str | None = None
+
+    freezer_number: int
+    rack_number: int
+    box_number: int
+
+    x_pos: int
+    y_pos: int
+
+@app.post("/add-vial")
+
+def add_vial(vial: VialCreate):
+    cur = conn.cursor()
+
+    cur.execute("""
+                SELECT id
+                FROM cell_lines
+                WHERE name = %s
+                """, (vial.cell_line,))
+    
+    result = cur.fetchone()
+
+    if result is None:
+        cur.execute("""
+                    INSERT INTO cell_lines (name)
+                    VALUES (%s)
+                    RETURNING id
+                    """, (vial.cell_line,))
+        
+        cell_line_id = cur.fetchone()[0]
+
+    else:
+        cell_line_id = result[0]
+
+    cur.execute("""
+                INSERT INTO frozen_samples (
+                    cell_line_id,
+                    freeze_passage_number,
+                    frozen_at,
+                    frozen_by,
+                
+                    freezer_number,
+                    rack_number,
+                    box_number,
+                
+                    x_pos,
+                    y_pos,
+                
+                    notes
+                )
+                
+                VALUES (
+                    %s, %s, %s, %s,
+                    %s, %s, %s,
+                    %s, %s,
+                    %s
+                )
+                """, (
+                    cell_line_id,
+                    vial.passage_number,
+                    datetime.now(),
+                    vial.frozen_by,
+
+                    vial.freezer_number,
+                    vial.rack_number,
+                    vial.box_number,
+
+                    vial.x_pos,
+                    vial.y_pos,
+
+                    vial.notes
+                ))
+    conn.commit()
+    return {"success": True}
--- static/JS/main.js
+++ static/JS/main.js
@@ -1,20 +1,42 @@
+let selectedRack =1;
+let selectedBox = 1;
+
 const rackButtons = document.querySelectorAll(".rack-button");
 
 rackButtons.forEach(button => {
     button.addEventListener("click", () => {
+        selectedRack = button.dataset.rack;
         rackButtons.forEach(b => {
             b.classList.remove("selected");
         });
         button.classList.add("selected");
+        renderBoxes();
     });
 });
+
+const boxButtons = document.querySelectorAll(".box-button");
+
+boxButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        selectedBox = button.dataset.box;
+        boxButtons.forEach(b => {
+            b.classList.remove("selected");
+        });
+        button.classList.add("selected");
+    })
+})
 
 const cells = document.querySelectorAll(".grid-cell");
 
 const modal = document.querySelector("#vial-modal");
 
+let selectedX = null;
+let selectedY = null;
+
 cells.forEach(cell => {
     cell.addEventListener("click", () => {
+        selectedX = cell.dataset.x;
+        selectedY = cell.dataset.y;
         modal.classList.remove("hidden");
     })
 })
@@ -24,3 +46,101 @@ const closeButton = document.querySelector("#close-modal");
 closeButton.addEventListener("click", () => {
     modal.classList.add("hidden");
 })
+
+const form = document.querySelector("#vial-form");
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+
+    const data ={
+        cell_line: formData.get("cell_line"),
+        passage_number: formData.get("passage_number"),
+        frozen_by: formData.get("frozen_by"),
+        notes: formData.get("notes"),
+
+        x_pos: selectedX,
+        y_pos: selectedY,
+
+        freezer_number: 1,
+        rack_number: selectedRack,
+        box_number: selectedBox
+    }
+
+    const response = await fetch("/add-vial", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+    if(response.ok) {
+        alert("Vial added!");
+        modal.classList.add("hidden");
+    }
+});
+
+const freezerData ={
+    1: {
+        racks: {
+            1: {
+                boxes: [1,2,3,4,5]
+            },
+            2: {
+                boxes: [1,2,3,4,5]
+            },
+            3: {
+                boxes: [1,2,3,4,5]
+            },
+            4: {
+                boxes: [1,2,3,4,5]
+            }
+        }
+    }
+};
+
+function renderBoxes() {
+    const container = document.querySelector(".box-buttons-div");
+    container.innerHTML ="";
+
+    for (let i=1; i<=5; i++){
+        const button = document.createElement("button");
+        button.textContent = `Box ${i}`;
+        button.classList.add("box-button");
+        button.dataset.box = 1;
+        button.addEventListener("click", () => {
+            selectedBox = i;
+            renderGrid();
+        });
+        container.appendChild(button);
+    }
+}
+
+function renderGrid() {
+    const grid = document.querySelector(".box-grid");
+
+    grid.innerHTML= "";
+
+    for(let y=0; y<10; y++) {
+        for(let x=0; x<10; x++) {
+            const cell = document.createElement("button");
+            cell.classList.add("grid-cell");
+
+            cell.dataset.x = x;
+            cell.dataset.y = y;
+
+            cell.addEventListener("click", () => {
+                selectedX = x;
+                selectedY = y;
+
+                modal.classList.remove("hidden");
+            });
+            grid.appendChild(cell);
+        }
+    }
+}
+
+renderBoxes();
+renderGrid();
